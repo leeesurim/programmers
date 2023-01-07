aa87fc5c92c2b152c43ce1ff1feaaa64df90d8ef
leeesurim <108252916+leeesurim@users.noreply.github.com>
2023-01-07 16:21:00 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,11 +1,11 @@
 function solution(denum1, num1, denum2, num2) {
-    let topNum = num1 * denum2 + num2 * denum1;
-    let botNum = num1 * num2;
-    let maximum = 1;
-    for(let i = 1; i<= topNum; i++){
-        if(topNum % i === 0 && botNum % i === 0){
-            maximum = i
+    let denum = num1 * denum2 + num2 * denum1;
+    let num = num1 * num2;
+    let gcd = 1;
+    for(let i = 1; i<= denum; i++){
+        if(denum % i === 0 && num % i === 0){
+            gcd = i
         }
     }
-    return [topNum/maximum, botNum/maximum];
+    return [denum/gcd , num/gcd];
 }
