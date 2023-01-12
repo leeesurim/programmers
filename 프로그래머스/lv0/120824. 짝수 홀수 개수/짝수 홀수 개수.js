83e88f77ceea3bf6a7e5d3ff45d833980ad1f6dd
leeesurim <108252916+leeesurim@users.noreply.github.com>
2023-01-12 22:29:16 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,6 +1,10 @@
 function solution(num_list) {
-    let even = 0;
-    let odd = 0;
-    num_list.map(num => num % 2 === 0 ? even+=1 : odd+=1)
-    return [even,odd];
+    // let even = 0;
+    // let odd = 0;
+    // num_list.map(num => num % 2 === 0 ? even+=1 : odd+=1);
+    // return [even,odd];
+    return [
+        num_list.filter(num => num % 2 === 0).length,
+        num_list.filter(num => num % 2 === 1).length
+    ]
 }
