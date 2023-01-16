42536ad819329a4e94ea5bef000122a5af9b95f5
leeesurim <108252916+leeesurim@users.noreply.github.com>
2023-01-16 22:34:45 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,16 +1,3 @@
 function solution(hp) {
-    var answer = 0;
-    while(hp!== 0){
-        if(Math.floor(hp/5) !== 0){
-            answer += Math.floor(hp/5);
-            hp = hp % 5;
-        } else if(Math.floor(hp/3) !== 0){
-            answer += Math.floor(hp/3);
-            hp = hp % 3;
-        } else{
-            answer += hp;
-            break;
-        }
-    }
-    return answer;
+    return Math.floor(hp/5) + Math.floor(hp % 5 / 3) + (hp % 5 % 3) ;
 }
