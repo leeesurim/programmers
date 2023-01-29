5a93af27cc38e96cf87209fcc37f9efca8392d4e
leeesurim <108252916+leeesurim@users.noreply.github.com>
2023-01-29 23:48:04 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,4 +1,5 @@
 function solution(num, k) {
     var answer = 0;
-    return String(num).indexOf(k) > -1 ? String(num).indexOf(k) + 1 : -1;
+    // return String(num).indexOf(k) > -1 ? String(num).indexOf(k) + 1 : -1;
+    return String(num).indexOf(k) + 1 || -1
 }
