ca72f8b569cc5533442e81414ed637b1732f4f03
leeesurim <108252916+leeesurim@users.noreply.github.com>
2023-02-28 00:33:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,11 +1,6 @@
 function solution(phone_number) {
     var answer = '';
     for(let i = 0; i < phone_number.length; i++){
-        if(i < phone_number.length - 4){
-            answer += "*"
-        }else{
-            answer += phone_number[i]
-        }
-    }
+        i < phone_number.length - 4 ? answer += "*" : answer += phone_number[i]}
     return answer;
 }
