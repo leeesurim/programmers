aa8ca54837d240d14b2f09d85cabcc623bc75bfb
leeesurim <108252916+leeesurim@users.noreply.github.com>
2023-03-17 23:26:47 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.56 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -4,12 +4,8 @@ process.stdin.on('data', data => {
     const a = Number(n[0]), b = Number(n[1]);
     // console.log(a);
     // console.log(b);
-    let answer = ""
-    for(let i = 0; i < b; i++){
-        for(let j = 0; j < a; j++){
-            answer += "*"
-        }
-        answer += "\n"
+    let row = "*".repeat(a);
+    for(let i=0; i<b; i++){
+        console.log(row)
     }
-    console.log(answer)
 });
